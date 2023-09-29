249bbfb74eddee2b925d421d831b26650efd6aa5
Tatewaki11 <144411796+Tatewaki11@users.noreply.github.com>
2023-09-29 13:40:48 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+let weight;
+let height;
+let BMI;
+
+weight = 65;
+height = 173;
+BMI = weight / ((height / 100) * (height / 100));
+
+console.log('体重:' + weight + 'kg')
+console.log('身長:' + height + 'cm')
+console.log('BMI:' + BMI)
